7fbbea7bea8f26cb4e4a1a50248b21eab3254b75
PolinaFaustova <polinamusikerin@gmail.com>
2023-04-01 15:10:23 +0200

    start working

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import Notiflix from 'notiflix';
+
+const searchForm = document.querySelector('#search-form');
+const inputForm = document.querySelector('input[type="text"]');
+const btnSubmit = document.querySelector('button[type="submit"]');
+
+searchForm.addEventListener('submit', event => {
+  event.preventDefault();
+  const searchQuery = inputForm.value.trim();
+});
