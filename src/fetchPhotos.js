d53f1155b6bb82dde735b4666bb7ca4010554d7d
PolinaFaustova <polinamusikerin@gmail.com>
2023-04-01 15:50:33 +0200

    imports

---
+++ src/fetchPhotos.js
@@ -0,0 +1,3 @@
+'use strict';
+import Notiflix from 'notiflix';
+const API_KEY = '34956140-3027e5abb464d64a1ffd53e5a';
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+'use strict';
 import Notiflix from 'notiflix';
 
 const searchForm = document.querySelector('#search-form');
