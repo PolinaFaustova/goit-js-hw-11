f12f64401182e9575398decb6050a95be18d4040
PolinaFaustova <polinamusikerin@gmail.com>
2023-04-02 22:00:45 +0200

    working on

--- src/PixabayAPI.js
+++ src/PixabayAPI.js
@@ -9,16 +9,20 @@ export class PixabayAPI {
   page = 1;
   query = null;
 
-  fetchPhoto() {
-    return axios.get(`${this.#BASE_URL}/search/photos`, {
-      params: {
-        key: this.#API_KEY,
-        q: this.query,
-        image_type: photo,
-        orientation: horizontal,
-        safesearch: true,
-        per_page: 40,
-      },
-    });
+  async fetchPhoto() {
+    try {
+      return axios.get(`${this.#BASE_URL}/search/photos`, {
+        params: {
+          key: this.#API_KEY,
+          q: this.query,
+          image_type: 'photo',
+          orientation: 'horizontal',
+          safesearch: true,
+          per_page: 40,
+        },
+      });
+    } catch (err) {
+      throw new Error(err.message);
+    }
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 'use strict';
 import { PixabayAPI } from './PixabayAPI';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import Notiflix from 'notiflix';
 
 const searchForm = document.querySelector('#search-form');
@@ -8,7 +10,75 @@ const btnSubmit = document.querySelector('button[type="submit"]');
 const galleryList = document.querySelector('.gallery');
 const btnLoadMore = document.querySelector('.load-more');
 
-searchForm.addEventListener('submit', event => {
+const pixabayApi = new PixabayAPI();
+
+const renderGalleryCard = ({
+  webformatURL,
+  largeImageURL,
+  tags,
+  likes,
+  views,
+  comments,
+  downloads,
+}) => {
+  return ` <div class='photo-card'>
+  //   <img src='${webformatURL}' alt='${tags}' loading='lazy' />
+  //   <div class='info'>
+  //     <p class='info-item'>
+  //       <b>Likes</b> ${likes}
+  //     </p>
+  //     <p class='info-item'>
+  //       <b>Views</b> ${views}
+  //     </p>
+  //     <p class='info-item'>
+  //       <b>Comments</b> ${comments}
+  //     </p>
+  //     <p class='info-item'>
+  //       <b>Downloads</b> ${downloads}
+  //     </p>
+  //   </div>
+  // </div>`;
+};
+
+const createGalleryCards = images => {
+  const galleryCards = images.map(renderGalleryCard);
+  return galleryCards.join('');
+};
+
+const handleSearchFormSubmit = async event => {
   event.preventDefault();
   const searchQuery = inputForm.value.trim();
-});
+  pixabayApi.query = searchQuery;
+
+  try {
+    const { data } = await pixabayApi.fetchPhoto();
+
+    if (!data.results.length) {
+      console.log('Images not found!');
+      return;
+    }
+    galleryList.innerHTML = createGalleryCards(data.results);
+    btnLoadMore.classList.remove('is-hidden');
+  } catch (err) {
+    console.log(err);
+  }
+};
+const handleBtnLoadMoreClick = async () => {
+  pixabayApi.page += 1;
+
+  try {
+    const { data } = await pixabayApi.fetchPhoto();
+
+    if (pixabayApi.page === data.totalPages) {
+      btnLoadMore.classList.add('is-hidden');
+    }
+    galleryList.insertAdjacentHTML(
+      'beforeend',
+      createGalleryCards(data.results)
+    );
+  } catch (err) {
+    console.log(err);
+  }
+};
+searchForm.addEventListener('submit', handleSearchFormSubmit);
+btnLoadMore.addEventListener('click', handleBtnLoadMoreClick);
